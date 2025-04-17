0700d31a7152cec78d3da585af185697e657c7f1
mercylin muthoni <muthonimercylin@gmail.com>
2025-04-17 10:31:14 +0300

    opening hours and menu dropdown

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const link = document.querySelectorAll('.navlink');
-link.forEach(item => {
-    item.addEventListener('click', () => {
-        e.preventDefault()
-        // document.querySelector('.navlink.active')?.classList.remove('active');
-        // item.classList.add('active');
 
-        link.forEach(element => element.classList.remove('active'));
-        this.classList.add('active')
-    });
-});
+function toggleDropdown() {
+    e.preventDefault()
+    document.getElementsByClassName("fa-bars").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it and click the links inside the dropdown
+window.onclick = function (event) {
+    if (!event.target.matches('.fa-bars')) {
+        var dropdowns = document.getElementsByClassName("vertical-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
